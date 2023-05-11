9384d3ff49b2ef6a0f7de3db610d0fe1d24e857a
JackCHG <gongjackiegong@gmail.com>
2023-05-11 01:45:39 -0700

    Added setup for heroku

---
+++ src/definitions/constants.d.ts
@@ -0,0 +1,2 @@
+export const _portNum = 8080;
+export const _mongoDatabasename = 'MERN-Boilerplate';
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import express = require('express');
 import mongoose = require('mongoose');
 import path = require('path');
 
+import * as constants from './definitions/constants';
+
 const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
@@ -14,14 +16,15 @@ import { testRouter } from './endpoints/test/test';
 app.use('/test', testRouter);
 
 async function initServer() {
-  const MONGODB_NAME = 'boilerplate';
-  await mongoose.connect('mongodb://127.0.0.1:27017/' + MONGODB_NAME);
+  const MONGODB_URI = process.env.MONGODB_URI
+                      || 'mongodb://127.0.0.1:27017/'
+                         + constants._mongoDatabaseName);
   console.log('Mongoose successfully connected to MongoDB.');
 
-  const PORT_NUM = 8080;
+  const PORT = process.env.PORT || constants._portNum;
   app.listen(PORT_NUM, function() {
-    console.log('Listening at http://127.0.0.1:' + PORT_NUM
-                + ' exporting the directory ' + __dirname + '.');
+    console.log('Listening at http://127.0.0.1:' + PORT
+                + ' exporting the directory ' + __dirname + '/path.');
   });
 }
 initServer();
